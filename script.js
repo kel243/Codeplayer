f93ea9f47cc831a5f2569fe85587fdc5600c8e6a
kel243 <54601458+kel243@users.noreply.github.com>
2020-01-06 19:36:45 -0500

    Restructured

---
+++ script.js
@@ -0,0 +1,59 @@
+function updateOutput() {
+    
+    $("iframe").contents().find("html").html("<html><head><style type='text/css'>" + $("#cssPanel").val() + "</style></head><body>" + $("#htmlPanel").val() + "</body></html>");
+
+    document.getElementById("outputPanel").contentWindow.eval($("#jsPanel").val());
+    
+}
+
+
+$(".toggleBtn").hover(function() {
+
+    $(this).addClass("highlighted");
+
+},  function() {
+
+    $(this).removeClass("highlighted");
+
+});
+
+$(".clearBtn").click(function() {
+    
+    $("#htmlPanel").val(" ");
+    
+    $("#cssPanel").val(" ");
+    
+    $("#jsPanel").val(" ");
+    
+    updateOutput();
+    
+});
+
+$(".toggleBtn").click(function() {
+
+    $(this).toggleClass("active-" + $(this).attr("id"));
+
+    $(this).removeClass("highlighted");
+
+    var panelID = $(this).attr("id") + "Panel";
+
+    $("#" + panelID).toggleClass("hidden");
+
+    var numOfActivePanels = 4 - $(".hidden").length;
+
+    var percentage = 100 / numOfActivePanels - 1;
+    $(".panel").width(`${percentage}%`);
+
+});
+
+$(".panel").height('93vh');
+
+$(".panel").width('49%');
+
+updateOutput();
+
+$("textarea").on('change keyup paste', function() {
+
+    updateOutput();
+
+})
